ad1f65e77360f6c997687fed24c878fdb87e3d39
mvproject <awsom7@gmail.com>
2022-01-15 17:28:54 +0200

    partially update minor fix

--- app/src/main/java/com/mvproject/tvprogramguide/repository/ChannelProgramRepository.kt
+++ app/src/main/java/com/mvproject/tvprogramguide/repository/ChannelProgramRepository.kt
@@ -4,30 +4,34 @@ import androidx.room.Transaction
 import com.mvproject.tvprogramguide.database.dao.ProgramDao
 import com.mvproject.tvprogramguide.model.data.Program
 import com.mvproject.tvprogramguide.netwotk.EpgService
+import com.mvproject.tvprogramguide.utils.COUNT_ZERO
 import com.mvproject.tvprogramguide.utils.Mappers.asProgramEntities
 import com.mvproject.tvprogramguide.utils.Mappers.asProgramFromEntities
-import timber.log.Timber
 import javax.inject.Inject
 
 class ChannelProgramRepository @Inject constructor(
     private val epgService: EpgService,
     private val programDao: ProgramDao
 ) {
-    init {
-        Timber.d("Injection ChannelProgramRepository")
-    }
-
     suspend fun loadPrograms(channels: List<String>): List<Program> {
         return programDao.getSelectedChannelPrograms(channels)
             .asProgramFromEntities()
             .filter { it.dateTimeEnd > System.currentTimeMillis() }
     }
 
+    suspend fun loadProgramsCount(channels: List<String>): Int {
+        return programDao.getSelectedChannelPrograms(channels).groupBy { it.channelId }.keys.count()
+    }
+
     @Transaction
     suspend fun loadProgram(id: String) {
         val ch = epgService.getChannelProgram(id).ch_programme
         val entities = ch.asProgramEntities(id)
-        programDao.deletePrograms(id)
-        programDao.insertPrograms(entities)
+        if (entities.count() > COUNT_ZERO) {
+            programDao.apply {
+                deletePrograms(id)
+                insertPrograms(entities)
+            }
+        }
     }
 }
