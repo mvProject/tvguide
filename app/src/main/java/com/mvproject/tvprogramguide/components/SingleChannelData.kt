61cc74b71afabf9c4ad81a1e7b40418a126cc4de
mvproject <awsom7@gmail.com>
2022-03-23 12:34:02 +0200

    refactor percent calculating update adapter add SingleChannelData item with sticky headers

---
+++ app/src/main/java/com/mvproject/tvprogramguide/components/SingleChannelData.kt
@@ -0,0 +1,36 @@
+package com.mvproject.tvprogramguide.components
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.mvproject.tvprogramguide.model.data.SingleChannelModel
+import com.mvproject.tvprogramguide.utils.Utils.calculateProgramProgress
+import com.mvproject.tvprogramguide.utils.Utils.convertTimeToReadableFormat
+
+@ExperimentalMaterialApi
+@ExperimentalFoundationApi
+@Composable
+fun SingleChannelData(singleChannelPrograms: List<SingleChannelModel>) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxHeight()
+    ) {
+        singleChannelPrograms.forEach {
+            stickyHeader {
+                DateItem(date = it.date)
+            }
+            items(it.programs) { program ->
+                ProgramItem(
+                    prgTime = program.dateTimeStart.convertTimeToReadableFormat(),
+                    prgTitle = program.title,
+                    prgDescription = program.description,
+                    progressValue = calculateProgramProgress(program.dateTimeStart,program.dateTimeEnd)
+                )
+            }
+        }
+    }
+}
