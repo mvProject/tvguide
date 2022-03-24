50bf40048734087757eb17d4b0eb19dfb9122fd9
mvproject <awsom7@gmail.com>
2022-03-24 18:25:24 +0200

    refactor AppSettingsFragment and AppSettingsViewModel with compose, events and state

--- app/src/main/java/com/mvproject/tvprogramguide/settings/AppSettingsViewModel.kt
+++ app/src/main/java/com/mvproject/tvprogramguide/settings/AppSettingsViewModel.kt
@@ -2,8 +2,12 @@ package com.mvproject.tvprogramguide.settings
 
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mvproject.tvprogramguide.helpers.StoreHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,26 +15,8 @@ class AppSettingsViewModel @Inject constructor(
     private val storeHelper: StoreHelper
 ) : ViewModel() {
 
-    fun setSelectedTheme(themeMode: Int) {
-        AppCompatDelegate.setDefaultNightMode(themeMode)
-        storeHelper.setSelectedTheme(themeMode)
-    }
-
-    fun setSelectedLanguage(language: String) {
-        storeHelper.setSelectedLanguage(language)
-    }
-
-    fun setProgramsCount(count: Int) {
-        storeHelper.setProgramByChannelDefaultCount(count)
-    }
-
-    fun setChannelsUpdatePeriod(period: Int) {
-        storeHelper.setChannelsUpdatePeriod(period)
-    }
-
-    fun setProgramsUpdatePeriod(period: Int) {
-        storeHelper.setProgramsUpdatePeriod(period)
-    }
+    private val _uiEvent = Channel<AppSettingState>()
+    val uiEvent = _uiEvent.receiveAsFlow()
 
     val selectedThemeMode
         get() = storeHelper.selectedTheme
@@ -46,4 +32,55 @@ class AppSettingsViewModel @Inject constructor(
 
     val programsViewCount
         get() = storeHelper.programByChannelDefaultCount
+
+    private val languageOptions = listOf(AppLang.English, AppLang.Russian, AppLang.Ukrainian)
+    val languageOptionsNames = languageOptions.map { it.name }
+
+    fun getLanguageDefaultSelectedIndex(selected: String) =
+        languageOptions.indexOfFirst { it.locale == selected }
+
+    val themeOptions = listOf(AppTheme.Light(), AppTheme.Dark(), AppTheme.System())
+
+    fun getThemeDefaultSelectedIndex(selected: Int) =
+        themeOptions.indexOfFirst { it.value == selected }
+
+    private fun setSelectedTheme(selected: Int) {
+        val themeMode = themeOptions[selected].value
+        AppCompatDelegate.setDefaultNightMode(themeMode)
+        storeHelper.setSelectedTheme(themeMode)
+    }
+
+    private fun setSelectedLanguage(language: String) {
+        languageOptions.firstOrNull { it.name == language }?.locale?.let { locale ->
+            storeHelper.setSelectedLanguage(locale)
+        }
+    }
+
+    fun onEvent(event: AppSettingEvent) {
+        when (event) {
+            is AppSettingEvent.OnChannelUpdatePeriodChange -> {
+                storeHelper.setChannelsUpdatePeriod(event.period)
+            }
+            is AppSettingEvent.OnProgramUpdatePeriodChange -> {
+                storeHelper.setProgramsUpdatePeriod(event.period)
+            }
+            is AppSettingEvent.OnProgramVisibleCountChange -> {
+                storeHelper.setProgramByChannelDefaultCount(event.count)
+            }
+            is AppSettingEvent.OnLanguageChange -> {
+                setSelectedLanguage(event.selectedLanguage)
+                sendEvent(AppSettingState.UpdateUI)
+            }
+            is AppSettingEvent.OnThemeChange -> {
+                setSelectedTheme(event.selectedTheme)
+                sendEvent(AppSettingState.UpdateUI)
+            }
+        }
+    }
+
+    private fun sendEvent(event: AppSettingState) {
+        viewModelScope.launch {
+            _uiEvent.send(event)
+        }
+    }
 }
