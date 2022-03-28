b1683db892e5d1cdb5146d38a7bcf2228bc93949
mvproject <awsom7@gmail.com>
2022-03-28 13:08:06 +0300

    update tabs channels with compose

--- app/src/main/java/com/mvproject/tvprogramguide/customlists/CustomListFragment.kt
+++ app/src/main/java/com/mvproject/tvprogramguide/customlists/CustomListFragment.kt
@@ -4,24 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
-import com.mvproject.tvprogramguide.R
-import com.mvproject.tvprogramguide.customlists.OrderCategoryAdapter.Companion.SELECTED_CHANNELS
-import com.mvproject.tvprogramguide.databinding.FragmentCustomListBinding
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.mvproject.tvprogramguide.components.TabMainScreen
+import com.mvproject.tvprogramguide.databinding.FragmentCustomListComposeBinding
+import com.mvproject.tvprogramguide.theme.TvGuideTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @ExperimentalMaterialApi
 @ExperimentalFoundationApi
+@ExperimentalPagerApi
 @AndroidEntryPoint
 class CustomListFragment : Fragment() {
-    private var _binding: FragmentCustomListBinding? = null
+    private var _binding: FragmentCustomListComposeBinding? = null
     private val binding get() = _binding!!
 
     private val customListViewModel: CustomListViewModel by viewModels()
@@ -33,7 +32,7 @@ class CustomListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentCustomListBinding.inflate(inflater, container, false)
+        _binding = FragmentCustomListComposeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -42,24 +41,11 @@ class CustomListFragment : Fragment() {
 
         customListViewModel.setSelectedList(params.listFoEdit)
 
-        with(binding) {
-            OrderCategoryAdapter(requireActivity() as AppCompatActivity).also {
-                categoryPager.adapter = it
+        binding.root.setContent {
+            TvGuideTheme {
+                TabMainScreen()
             }
-
-            // set proper title to tab
-            TabLayoutMediator(
-                categoryPagerHeader,
-                categoryPager
-            ) { tab: TabLayout.Tab, position: Int ->
-                tab.text = getPageTitle(position)
-            }.attach()
         }
-    }
-
-    private fun getPageTitle(position: Int) = when (position) {
-        SELECTED_CHANNELS -> resources.getText(R.string.selected_channels_title)
-        else -> resources.getText(R.string.all_channels_title)
     }
 
     override fun onDestroyView() {
