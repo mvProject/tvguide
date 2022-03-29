b74a65e5c7f4b6eb4d2879102cc38c6a483e0cc9
mvproject <awsom7@gmail.com>
2022-03-29 18:53:35 +0300

    add ToolbarWithOptions add ChannelList

---
+++ app/src/main/java/com/mvproject/tvprogramguide/components/ChannelList.kt
@@ -0,0 +1,52 @@
+package com.mvproject.tvprogramguide.components
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.mvproject.tvprogramguide.model.data.Channel
+import com.mvproject.tvprogramguide.model.data.SelectedChannelModel
+import com.mvproject.tvprogramguide.utils.Utils.calculateProgramProgress
+import com.mvproject.tvprogramguide.utils.Utils.convertTimeToReadableFormat
+import com.mvproject.tvprogramguide.utils.Utils.parseChannelName
+
+@ExperimentalMaterialApi
+@ExperimentalFoundationApi
+@Composable
+fun ChannelList(
+    singleChannelPrograms: List<SelectedChannelModel>,
+    onChannelClick: (Channel) -> Unit
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(Color.White)
+    ) {
+        singleChannelPrograms.forEach {
+            stickyHeader {
+                ChannelItem(
+                    channelName = it.channel.channelName.parseChannelName(),
+                    channelLogo = it.channel.channelIcon
+                ) {
+                    onChannelClick(it.channel)
+                }
+            }
+            items(it.programs) { program ->
+                ProgramItem(
+                    prgTime = program.dateTimeStart.convertTimeToReadableFormat(),
+                    prgTitle = program.title,
+                    prgDescription = program.description,
+                    progressValue = calculateProgramProgress(
+                        program.dateTimeStart,
+                        program.dateTimeEnd
+                    )
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/mvproject/tvprogramguide/components/ToolbarWithOptions.kt
@@ -0,0 +1,62 @@
+package com.mvproject.tvprogramguide.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.mvproject.tvprogramguide.theme.appColors
+import com.mvproject.tvprogramguide.theme.dimens
+
+@Composable
+fun ToolbarWithOptions(
+    title: String,
+    onSelectClick: () -> Unit,
+    onSettingsClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize()
+            .background(color = MaterialTheme.appColors.backgroundPrimary)
+            .padding(MaterialTheme.dimens.size8),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Text(
+            text = title,
+            modifier = Modifier
+                .weight(1f)
+                .clickable {
+                    onSelectClick()
+                },
+            color = MaterialTheme.appColors.textPrimary,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.padding(horizontal = MaterialTheme.dimens.size8))
+
+        Icon(
+            Icons.Outlined.Settings,
+            contentDescription = null,
+            tint = MaterialTheme.appColors.textPrimary,
+            modifier = Modifier.clickable {
+                onSettingsClick()
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ToolbarWithOptionsView() {
+    ToolbarWithOptions("TetstTitle", onSelectClick = {}, onSettingsClick = {})
+}
