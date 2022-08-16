94f16341d0341a80fa85b9fab1f9c6b638bd84ec
mvproject <awsom7@gmail.com>
2022-08-16 14:42:12 +0300

    add onboard graphics update onboard color settings

--- app/src/main/java/com/mvproject/tvprogramguide/ui/onboard/OnBoardingPage.kt
+++ app/src/main/java/com/mvproject/tvprogramguide/ui/onboard/OnBoardingPage.kt
@@ -12,21 +12,20 @@ sealed class OnBoardingPage(
     @StringRes
     val description: Int
 ) {
-    // todo set proper graphic resources
     object First : OnBoardingPage(
-        image = R.drawable.ic_tvguide_logo,
+        image = R.drawable.onboard_user_list_create,
         title = R.string.onboard_title_user_list_create,
         description = R.string.onboard_description_user_list_create
     )
 
     object Second : OnBoardingPage(
-        image = R.drawable.ic_tvguide_logo,
+        image = R.drawable.onboard_user_list_switch,
         title = R.string.onboard_title_user_list_switch,
         description = R.string.onboard_description_user_list_switch
     )
 
     object Third : OnBoardingPage(
-        image = R.drawable.ic_tvguide_logo,
+        image = R.drawable.onboard_notification,
         title = R.string.onboard_title_notification,
         description = R.string.onboard_description_notification
     )
