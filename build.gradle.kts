bd3414db101b95d5e110865cceb33749cd023d16
mvproject <awsom7@gmail.com>
2022-08-19 15:23:42 +0300

    add plugin management

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,13 +31,6 @@ ktlint {
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
