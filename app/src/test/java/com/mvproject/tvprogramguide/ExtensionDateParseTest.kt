8b829adc26467f76468fd76eed33cf8475babf72
mvproject <awsom7@gmail.com>
2022-09-01 10:07:20 +0300

    add tests for extensions

---
+++ app/src/test/java/com/mvproject/tvprogramguide/ExtensionDateParseTest.kt
@@ -0,0 +1,68 @@
+package com.mvproject.tvprogramguide
+
+import com.mvproject.tvprogramguide.data.utils.convertDateToReadableFormat
+import com.mvproject.tvprogramguide.data.utils.convertTimeToReadableFormat
+import com.mvproject.tvprogramguide.data.utils.toMillis
+import io.kotest.assertions.withClue
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.longs.shouldBeGreaterThan
+import io.kotest.matchers.longs.shouldNotBeLessThanOrEqual
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+
+class ExtensionDateParseTest : StringSpec({
+    val testDateText = "29-08-2022 12:15"
+    val testDateLong = 1661764500000L
+    val testDateShort = "29 08 2022"
+    val testTimeShort = "12:15"
+
+    "textDateParseTest" {
+        withClue("text date parse") {
+            withClue("parse date to long") {
+                testDateText.toMillis() shouldBe testDateLong
+            }
+            withClue("parse date to not non positive long") {
+                testDateText.toMillis() shouldNotBeLessThanOrEqual 0L
+            }
+            withClue("parse date to non zero long") {
+                testDateText.toMillis() shouldBeGreaterThan 0L
+            }
+        }
+    }
+
+    "longDateParseTest" {
+        withClue("long date parse to date") {
+            withClue("parse long date to short text") {
+                testDateLong.convertDateToReadableFormat() shouldBe testDateShort
+            }
+            withClue("parse long date not empty or some") {
+                testDateLong.convertDateToReadableFormat() shouldNotBe ""
+                testDateLong.convertDateToReadableFormat() shouldNotBe "Some String"
+            }
+            withClue("parse zero long to date not empty") {
+                0L.convertDateToReadableFormat() shouldNotBe ""
+            }
+
+            withClue("parse not date long to date not empty") {
+                1000L.convertDateToReadableFormat() shouldNotBe ""
+            }
+        }
+
+        withClue("long date parse to time") {
+            withClue("parse long date to short text") {
+                testDateLong.convertTimeToReadableFormat() shouldBe testTimeShort
+            }
+            withClue("parse long date not empty or some") {
+                testDateLong.convertTimeToReadableFormat() shouldNotBe ""
+                testDateLong.convertTimeToReadableFormat() shouldNotBe "Some String"
+            }
+            withClue("parse zero long to time not empty") {
+                0L.convertTimeToReadableFormat() shouldNotBe ""
+            }
+
+            withClue("parse not date long to time not empty") {
+                1000L.convertTimeToReadableFormat() shouldNotBe ""
+            }
+        }
+    }
+})
---
+++ app/src/test/java/com/mvproject/tvprogramguide/ExtensionTextParseTest.kt
@@ -0,0 +1,55 @@
+package com.mvproject.tvprogramguide
+
+import com.mvproject.tvprogramguide.data.utils.manageLength
+import com.mvproject.tvprogramguide.data.utils.parseChannelName
+import io.kotest.assertions.withClue
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class ExtensionTextParseTest : StringSpec({
+    val chnOneDelimiter = "PartOne • PartOne"
+    val chnTwoDelimiter = "PartOne • PartOne • PartOne"
+    val chnOtherDelimiter = "PartOne * PartOne"
+    val chnNoDelimiter = "PartOne PartOne"
+
+    val longName = "SomeVeryVeryLongNameForSomething"
+    val shortName = "SomeShortName"
+    val emptyName = ""
+
+    "delimiterTest" {
+        withClue("channel name parse") {
+            withClue("no delimiter") {
+                chnNoDelimiter.parseChannelName() shouldBe chnNoDelimiter
+            }
+            withClue("not specified delimiter") {
+                chnOtherDelimiter.parseChannelName() shouldBe chnOtherDelimiter
+            }
+            withClue("with one delimiter") {
+                chnOneDelimiter.parseChannelName() shouldBe "PartOne"
+            }
+            withClue("with two delimiters") {
+                chnTwoDelimiter.parseChannelName() shouldBe "PartOne"
+            }
+            withClue("empty name") {
+                "".parseChannelName() shouldBe ""
+            }
+        }
+    }
+
+    "manage string length" {
+        withClue("some name parse") {
+            withClue("manage empty name") {
+                emptyName.manageLength() shouldBe emptyName
+                emptyName.manageLength().length shouldBe 0
+            }
+            withClue("manage shortName") {
+                shortName.manageLength() shouldBe shortName
+                shortName.manageLength().length shouldBe 13
+            }
+            withClue("manage LongName") {
+                longName.manageLength() shouldBe "SomeVeryVeryLongName"
+                longName.manageLength().length shouldBe 20
+            }
+        }
+    }
+})
