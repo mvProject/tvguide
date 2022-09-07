1878f6437948ea503ac2a969284411bda7467ac3
mvproject <awsom7@gmail.com>
2022-09-07 12:29:32 +0300

    update and organize already created tests

--- app/src/test/java/com/mvproject/tvprogramguide/repository/CustomListRepositoryTest.kt
+++ app/src/test/java/com/mvproject/tvprogramguide/repository/CustomListRepositoryTest.kt
@@ -6,99 +6,114 @@ import com.mvproject.tvprogramguide.data.model.entity.UserChannelsListEntity
 import com.mvproject.tvprogramguide.data.repository.CustomListRepository
 import io.kotest.assertions.assertSoftly
 import io.kotest.assertions.withClue
-import io.kotest.common.runBlocking
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.equality.shouldBeEqualToComparingFields
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import io.mockk.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.runTest
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class CustomListRepositoryTest : StringSpec({
     assertSoftly {
-        withClue("test delete list") {
-            "delete list from database" {
-                val dao = mockk<UserChannelsListDao>()
-                val repository = CustomListRepository(dao)
 
-                coEvery {
-                    dao.deleteSingleUserChannelsList(1)
-                } just runs
+        "delete list from database" {
+            val dao = mockk<UserChannelsListDao>()
+            val repository = CustomListRepository(dao)
 
-                runBlocking {
-                    repository.deleteList(UserChannelsList(1, "1"))
-                }
+            coEvery {
+                dao.deleteSingleUserChannelsList(1)
+            } just runs
+
+            withClue("single call from dao execute") {
+                repository.deleteList(UserChannelsList(1, "1"))
 
                 coVerify(exactly = 1) {
                     dao.deleteSingleUserChannelsList(1)
                 }
+                confirmVerified(dao)
             }
         }
 
-        withClue("test add list proper name") {
-            "add list to database if name filled" {
-                val repository = mockk<CustomListRepository>()
+        "add list to database if name filled" {
+            val repository = mockk<CustomListRepository>()
 
-                coEvery {
-                    repository.addCustomList("list")
-                } just runs
+            coEvery {
+                repository.addCustomList("list")
+            } just runs
 
-                runBlocking {
-                    repository.addCustomList("list")
-                }
+            withClue("single call from repository execute") {
+                repository.addCustomList("list")
+
                 coVerify(exactly = 1) {
                     repository.addCustomList("list")
                 }
+                confirmVerified(repository)
             }
         }
 
-        withClue("test add list wrong name") {
-            "add list to database if name empty" {
-                val dao = mockk<UserChannelsListDao>(relaxed = true)
-                val repository = CustomListRepository(dao)
+        "add list to database if name empty" {
+            val dao = mockk<UserChannelsListDao>(relaxed = true)
+            val repository = CustomListRepository(dao)
 
-                coEvery {
-                    dao.addUserChannelsList(UserChannelsListEntity(1, ""))
-                } just runs
+            coEvery {
+                dao.addUserChannelsList(UserChannelsListEntity(1, ""))
+            } just runs
 
-                runBlocking {
-                    repository.addCustomList("")
-                }
+            withClue("single call from dao execute") {
+                repository.addCustomList("")
+
                 coVerify(exactly = 0) {
                     dao.addUserChannelsList(UserChannelsListEntity(1, ""))
                 }
+                confirmVerified(dao)
             }
         }
 
-        withClue("get all lists") {
-            "retrieve userlists" {
-                val dao = mockk<UserChannelsListDao>()
-                val repository = CustomListRepository(dao)
+        "retrieve userlists" {
+            val dao = mockk<UserChannelsListDao>()
+            val repository = CustomListRepository(dao)
 
-                val resultListEntity = listOf(
-                    UserChannelsListEntity(1, "list1"),
-                    UserChannelsListEntity(2, "list2"),
-                    UserChannelsListEntity(3, "list3")
-                )
+            val expectedResultDao = listOf(
+                UserChannelsListEntity(1, "list1"),
+                UserChannelsListEntity(2, "list2"),
+                UserChannelsListEntity(3, "list3")
+            )
 
-                val resultList = listOf(
-                    UserChannelsList(1, "list1"),
-                    UserChannelsList(2, "list2"),
-                    UserChannelsList(3, "list3")
-                )
+            val expectedResult = listOf(
+                UserChannelsList(1, "list1"),
+                UserChannelsList(2, "list2"),
+                UserChannelsList(3, "list3")
+            )
 
-                coEvery {
+            coEvery {
+                dao.getAllUserChannelsLists()
+            } returns flow {
+                emit(expectedResultDao)
+            }
+
+            withClue("single call from dao execute") {
+                repository.loadChannelsLists()
+
+                coVerify(exactly = 1) {
                     dao.getAllUserChannelsLists()
-                } returns flow {
-                    emit(resultListEntity)
                 }
+                confirmVerified(dao)
+            }
 
-                runBlocking {
-                    repository.loadChannelsLists().collect { list ->
-                        list.shouldBeInstanceOf<List<UserChannelsList>>()
-                        list.first() shouldBeEqualToComparingFields resultList.first()
-                        list.last().listName shouldBe "list3"
-                        list.last().id shouldBe 3
+            withClue("collect proper data from flow") {
+                runTest {
+                    repository.loadChannelsLists().collect { retrievedResult ->
+                        withClue("result is list of entity value") {
+                            retrievedResult.shouldBeInstanceOf<List<UserChannelsList>>()
+                            retrievedResult.first() shouldBeEqualToComparingFields expectedResult.first()
+                        }
+                        withClue("result fields values match expected values") {
+                            retrievedResult.last().listName shouldBe "list3"
+                            retrievedResult.last().id shouldBe 3
+                        }
                     }
                 }
             }
