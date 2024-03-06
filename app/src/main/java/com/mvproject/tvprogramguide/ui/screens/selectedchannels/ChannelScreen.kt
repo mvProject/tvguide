921e17c071d5b800e3073e91529b43fa836ef7b7
mvproject <awsom7@gmail.com>
2024-03-06 17:21:16 +0200

    add scroll to top button add toolbar behaviour

--- app/src/main/java/com/mvproject/tvprogramguide/ui/screens/selectedchannels/ChannelScreen.kt
+++ app/src/main/java/com/mvproject/tvprogramguide/ui/screens/selectedchannels/ChannelScreen.kt
@@ -5,16 +5,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -26,9 +33,11 @@ import com.mvproject.tvprogramguide.ui.components.channels.ChannelList
 import com.mvproject.tvprogramguide.ui.components.dialogs.ShowSelectFromListDialog
 import com.mvproject.tvprogramguide.ui.components.toolbars.ToolbarWithOptions
 import com.mvproject.tvprogramguide.ui.components.views.NoItemsScreen
+import com.mvproject.tvprogramguide.utils.AppConstants.COUNT_ZERO
 import com.mvproject.tvprogramguide.utils.AppConstants.REFRESH_DELAY
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalCoroutinesApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -61,20 +70,48 @@ fun ChannelScreen(
         }
     }
 
+    val scope = rememberCoroutineScope()
+
+    val showScrollToTopButton by remember {
+        derivedStateOf {
+            listState.firstVisibleItemIndex >= 5
+        }
+    }
+
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
         containerColor = MaterialTheme.colorScheme.inverseOnSurface,
         contentColor = MaterialTheme.colorScheme.onSurface,
         topBar = {
             ToolbarWithOptions(
                 title = viewState.listName,
                 isSelectEnabled = viewState.isNotSinglePlaylist,
+                scrollBehavior = scrollBehavior,
                 onSelectClick = {
                     isDialogOpen.value = true
                 },
                 onSettingsClick = onNavigateSettings
             )
+        },
+        floatingActionButton = {
+            if (showScrollToTopButton) {
+                FloatingActionButton(onClick = {
+                    scope.launch {
+                        listState.animateScrollToItem(COUNT_ZERO)
+                    }
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowUp,
+                        contentDescription = null
+                    )
+                }
+            }
         }
+
     ) { padding ->
         when {
             viewState.listName.isEmpty() -> {
